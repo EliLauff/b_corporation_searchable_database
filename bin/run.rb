7c9676207a792fe3a0e7312a6ab017e38e2a19e7
EliLauff <eli.lauffenburger@gmail.com>
2019-03-06 11:32:01 -0600

    experimenting with CSV methods

--- bin/run.rb
+++ bin/run.rb
@@ -1,5 +1,9 @@
+require 'pry'
 require_relative '../config/environment'
-
-
+puts Dir.pwd
+# b_corp_table = CSV.parse('./lib/b_corp_impact_data.csv', headers: true)
+# binding.pry
+# puts b_corp_table[2]["company_name"]
+CSV.foreach("")
 
 puts "HELLO WORLD"
